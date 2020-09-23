33dd636fd97a9c054275c733573e5b2eb42b9ade
Huu Hung Phan <huuhung.phan@gmail.com>
2020-09-23 19:12:36 +0200

    Initial Commit

---
+++ PromotionEngine/UnitTestPromotionEngine/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestPromotionEngine
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void Test_Senario_A()
+    {
+    }
+    [TestMethod]
+    public void Test_Senario_B()
+    {
+    }
+  }
+}
